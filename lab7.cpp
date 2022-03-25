7990ed2633711a473e434206233f7204d4ed4151
TonySavich <92541859+TonySavich@users.noreply.github.com>
2022-03-25 21:25:47 +0200

    Update lab7.cpp

--- lab7.cpp
+++ lab7.cpp
@@ -10,9 +10,9 @@ class Test
 
 class Matrix
 {
-// Абстракция
-// Инкапсуляция
-// Использование вне класса
+	// Абстракция
+	// Инкапсуляция
+	// Использование вне класса
 public:
 	// Конструктор
 	Matrix(int n, int m)
@@ -20,7 +20,7 @@ public:
 		std::cout << "Constructor" << std::endl;
 		m_n = n;
 		m_m = m;
-		m_mat = new int* [m_n];
+		m_mat = new int*[m_n];
 		for (int i = 0; i < m_n; i++)
 			m_mat[i] = new int[m_m];
 	}
@@ -33,7 +33,7 @@ public:
 		m_n = mat.m_n;
 		m_m = mat.m_m;
 
-		m_mat = new int* [m_n];
+		m_mat = new int*[m_n];
 		for (int i = 0; i < m_n; i++)
 			m_mat[i] = new int[m_m];
 
@@ -80,15 +80,15 @@ public:
 	// Оператор умножения
 	Matrix operator*(const Matrix& mat) {
 		std::cout << "operator*" << std::endl;
-		Matrix tmp(mat.m_n, mat.m_m);
-		for (int i = 0; i < m_n; i++)
-			for (int j = 0; j < m_m; j++)
-				tmp.m_mat[i][j] = 0;
-		for (int i = 0; i < m_n; i++)
-			for (int q = 0; q < m_m; q++)
-				for (int j = 0; j < m_m; j++)
-				tmp.m_mat[i][q] = m_mat[i][j] * mat.m_mat[j][q] + tmp.m_mat[i][q];
-		return tmp;
+			Matrix tmp(m_n, mat.m_m);
+			for (int i = 0; i < m_n; i++)
+				for (int j = 0; j < mat.m_m; j++)
+					tmp.m_mat[i][j] = 0;
+			for (int i = 0; i < m_n; i++)
+				for (int q = 0; q < mat.m_m; q++)
+					for (int j = 0; j < mat.m_n; j++)
+						tmp.m_mat[i][q] = m_mat[i][j] * mat.m_mat[j][q] + tmp.m_mat[i][q];
+			return tmp;
 	}
 
 	// Оператор вычитания
@@ -113,81 +113,81 @@ public:
 
 
 
-	    int det(const Matrix & mat) {
-			std::cout << "determinal" << std::endl;
-			if ((m_n == 2 and m_m == 2) or (m_n == 3 and m_m == 3)) {
-				if (m_n == 2 and m_m == 2) {
-					int tmp;
-					tmp = m_mat[0][0] * m_mat[1][1] - m_mat[0][1] * m_mat[1][0];
-					return tmp;
-				}
-				else {
-					int tmp;
-					tmp = m_mat[0][0] * m_mat[1][1] * m_mat[2][2] + m_mat[1][0] * m_mat[2][1] * m_mat[0][2] + m_mat[0][1] * m_mat[1][2] * m_mat[2][0] - m_mat[0][2] * m_mat[1][1] * m_mat[2][0] - m_mat[0][0] * m_mat[1][2] * m_mat[2][1] - m_mat[0][1] * m_mat[1][0] * m_mat[2][2];
-					return tmp;
-				}
+	int det( ) {
+		std::cout << "determinal" << std::endl;
+		if ((m_n == 2 and m_m == 2) or (m_n == 3 and m_m == 3)) {
+			if (m_n == 2 and m_m == 2) {
+				int tmp;
+				tmp = m_mat[0][0] * m_mat[1][1] - m_mat[0][1] * m_mat[1][0];
+				return tmp;
 			}
 			else {
-				std::cout << "ERROR";
+				int tmp;
+				tmp = m_mat[0][0] * m_mat[1][1] * m_mat[2][2] + m_mat[1][0] * m_mat[2][1] * m_mat[0][2] + m_mat[0][1] * m_mat[1][2] * m_mat[2][0] - m_mat[0][2] * m_mat[1][1] * m_mat[2][0] - m_mat[0][0] * m_mat[1][2] * m_mat[2][1] - m_mat[0][1] * m_mat[1][0] * m_mat[2][2];
+				return tmp;
 			}
 		}
+		else {
+			std::cout << "ERROR";
+		}
+	}
 
 
-		Matrix inv(const Matrix& mat) {
-			std::cout << "inversion" << std::endl;
-			if ((m_n == 2 and m_m == 2) or (m_n == 3 and m_m == 3)) {
-				if (m_n == 2 and m_m == 2) {
-					Matrix tmp(2, 2);
+	Matrix inv( ) {
+		std::cout << "inversion" << std::endl;
+		if ((m_n == 2 and m_m == 2) or (m_n == 3 and m_m == 3)) {
+			if (m_n == 2 and m_m == 2) {
+				Matrix tmp(2, 2);
 
 
-					int k;
-					k = m_mat[0][0] * m_mat[1][1] - m_mat[0][1] * m_mat[1][0];
+				int k;
+				k = m_mat[0][0] * m_mat[1][1] - m_mat[0][1] * m_mat[1][0];
 
 
-					tmp.m_mat[0][0] = m_mat[1][1]/k;
-					tmp.m_mat[0][1] = m_mat[0][1]/k;
-					tmp.m_mat[1][0] = m_mat[1][0]/k;
-					tmp.m_mat[1][1] = m_mat[0][0]/k;
-					return tmp;
-				}
-				else {
-					Matrix tmp(3,3);
-					int k;
-					k = m_mat[0][0] * m_mat[1][1] * m_mat[2][2] + m_mat[1][0] * m_mat[2][1] * m_mat[0][2] + m_mat[0][1] * m_mat[1][2] * m_mat[2][0] - m_mat[0][2] * m_mat[1][1] * m_mat[2][0] - m_mat[0][0] * m_mat[1][2] * m_mat[2][1] - m_mat[0][1] * m_mat[1][0] * m_mat[2][2];
-
-
-					tmp.m_mat[0][0] = (m_mat[1][1] * m_mat[2][2] - m_mat[2][1] * m_mat[1][2])/k;
-					tmp.m_mat[1][0] = (m_mat[1][0] * m_mat[2][2] - m_mat[2][0] * m_mat[1][2])/k;
-					tmp.m_mat[2][0] = (m_mat[1][0] * m_mat[2][1] - m_mat[2][0] * m_mat[1][1])/k;
-					tmp.m_mat[0][1] = (m_mat[0][1] * m_mat[2][2] - m_mat[2][1] * m_mat[0][2])/k;
-					tmp.m_mat[1][1] = (m_mat[0][0] * m_mat[2][2] - m_mat[2][0] * m_mat[0][2])/k;
-					tmp.m_mat[2][1] = (m_mat[0][0] * m_mat[2][1] - m_mat[2][0] * m_mat[0][1])/k;
-					tmp.m_mat[0][2] = (m_mat[0][1] * m_mat[1][2] - m_mat[1][1] * m_mat[0][2])/k;
-					tmp.m_mat[1][2] = (m_mat[0][0] * m_mat[1][2] - m_mat[1][0] * m_mat[0][2])/k;
-					tmp.m_mat[2][2] = (m_mat[0][0] * m_mat[1][1] - m_mat[1][0] * m_mat[0][1])/k;
-				
-					return tmp;
-				}
+				tmp.m_mat[0][0] = m_mat[1][1] / k;
+				tmp.m_mat[0][1] = m_mat[0][1] / k;
+				tmp.m_mat[1][0] = m_mat[1][0] / k;
+				tmp.m_mat[1][1] = m_mat[0][0] / k;
+				return tmp;
 			}
 			else {
-				std::cout << "ERROR";
+				Matrix tmp(3, 3);
+				int k;
+				k = m_mat[0][0] * m_mat[1][1] * m_mat[2][2] + m_mat[1][0] * m_mat[2][1] * m_mat[0][2] + m_mat[0][1] * m_mat[1][2] * m_mat[2][0] - m_mat[0][2] * m_mat[1][1] * m_mat[2][0] - m_mat[0][0] * m_mat[1][2] * m_mat[2][1] - m_mat[0][1] * m_mat[1][0] * m_mat[2][2];
+
+
+				tmp.m_mat[0][0] = (m_mat[1][1] * m_mat[2][2] - m_mat[2][1] * m_mat[1][2]) / k;
+				tmp.m_mat[1][0] = (-m_mat[1][0] * m_mat[2][2] + m_mat[2][0] * m_mat[1][2]) / k;
+				tmp.m_mat[2][0] = (m_mat[1][0] * m_mat[2][1] - m_mat[2][0] * m_mat[1][1]) / k;
+				tmp.m_mat[0][1] = (-m_mat[0][1] * m_mat[2][2] + m_mat[2][1] * m_mat[0][2]) / k;
+				tmp.m_mat[1][1] = (m_mat[0][0] * m_mat[2][2] - m_mat[2][0] * m_mat[0][2]) / k;
+				tmp.m_mat[2][1] = (-m_mat[0][0] * m_mat[2][1] + m_mat[2][0] * m_mat[0][1]) / k;
+				tmp.m_mat[0][2] = (m_mat[0][1] * m_mat[1][2] - m_mat[1][1] * m_mat[0][2]) / k;
+				tmp.m_mat[1][2] = (-m_mat[0][0] * m_mat[1][2] + m_mat[1][0] * m_mat[0][2]) / k;
+				tmp.m_mat[2][2] = (m_mat[0][0] * m_mat[1][1] - m_mat[1][0] * m_mat[0][1]) / k;
+
+				return tmp;
 			}
 		}
-
-
-
-		Matrix tran(const Matrix& mat) {
-			std::cout << "transponirovanie" << std::endl;
-			Matrix tmp(mat.m_m, mat.m_n);
-			for (int i = 0; i < m_n; i++)
-				for (int j = 0; j < m_m; j++)
-					tmp.m_mat[j][i] = m_mat[i][j];
-			return tmp;
+		else {
+			std::cout << "ERROR";
 		}
+	}
 
-	
 
-	
+
+	Matrix tran() {
+		std::cout << "transponirovanie" << std::endl;
+		Matrix tmp(m_m, m_n);
+		for (int i = 0; i < m_n; i++)
+			for (int j = 0; j < m_m; j++)
+				tmp.m_mat[j][i] = m_mat[i][j];
+		return tmp;
+	}
+
+
+
+
 	// Деструктор
 	~Matrix()
 	{
@@ -201,7 +201,7 @@ public:
 	friend std::istream& operator>>(std::istream& os, Matrix& mat);
 	friend std::ostream& operator<<(std::ostream& os, const Matrix& mat);
 
-// Использование внутри класса
+	// Использование внутри класса
 private:
 	int m_n, m_m;		// Поле
 	int n;
@@ -239,17 +239,19 @@ int main()
 {
 	Test test;
 
-	Matrix A(3, 3);
+	Matrix A(3, 2);
 
 	std::cin >> A;
-	std::cout << A << std::endl; 
-	Matrix B(3, 3);
+	std::cout << A << std::endl;
+	Matrix B(2, 3);
 	std::cin >> B;
 	std::cout << B << std::endl;
 	Matrix C(3, 3);
 	C = A * B;
 	std::cout << C << std::endl;
-
+	Matrix Z(3, 3);
+	std::cin >> Z;
+	std::cout << Z.inv();
 
 
 	return 0;
